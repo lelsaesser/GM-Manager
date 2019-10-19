78f3202662080db3c7d08c9f82c7a2c8b5fb5c35
lelsaesser <lucas.elsaesser@gmail.com>
2019-10-19 17:40:58 +0200

    Frontend: initialized Angular project

--- src/scripts/start_flask_backend.sh
+++ src/scripts/start_flask_backend.sh
@@ -4,4 +4,4 @@
 export FLASK_APP="../GM-Manager-Flask/app.py"
 source /c/Anaconda3/etc/profile.d/conda.sh
 conda activate GM-Manager-Flask
-flask run -h 0.0.0.0
+flask run -h 0.0.0.0 # runs on 127.0.0.1:5000
