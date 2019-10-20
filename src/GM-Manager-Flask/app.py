0c52f081b1edadb94de90a7e24955759910a927f
lelsaesser <lucas.elsaesser@gmail.com>
2019-10-20 16:04:48 +0200

    Flask: created survrun get api endpoint. Scripts: added angular boot script

--- src/GM-Manager-Flask/app.py
+++ src/GM-Manager-Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, make_response
 from modes.survrim.survrun_goal_location_calculator import SurvrunGoalLocationCalculator
 from modes.stronghold.shc_ai_picker import StrongholdAiPicker
 
@@ -10,18 +10,34 @@ def hello_world():
     return 'GM-Manager'
 
 
-@app.route('/survrim')
-def survrim_route():
-    survrun_goals = SurvrunGoalLocationCalculator()
-    a, b = survrun_goals.calc_goal_locations()
-    return 'Survrun target locations: ' + a + " " + b
+@app.route('/api/survrim', methods=['GET'])
+def get_survrun_locations():
+    survrun_targets = SurvrunGoalLocationCalculator()
+    target_a, target_b = survrun_targets.calc_goal_locations()
+    if not target_a or not target_b:
+        abort(404)
+
+    survrun_data = [
+        {
+            'id': 1,
+            'target_location_one': target_a,
+            'target_location_two': target_b
+        }
+    ]
+
+    return jsonify({'survrun_data': survrun_data})
 
 
-@app.route('/shc')
+@app.route('/api/shc')
 def stronghold_route():
     ai_list = StrongholdAiPicker.pick_random_ai(8)
     ai_list_str = StrongholdAiPicker.format_ai_list(ai_list)
     return 'Stronghold random game: ' + ai_list_str
+
+
+@app.errorhandler(404)
+def error_not_found(error):
+    return make_response(jsonify({'error': '404: Not found'}), 404)
 
 
 if __name__ == '__main__':
--- src/scripts/start_angular_frontend.sh
+++ src/scripts/start_angular_frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "../GM-Manager-Angular"
-ng serve 
+ng serve # runs on http://localhost:4200/webpack-dev-server/
