acdcc5605a61eb3190edd4e940eff359623e89c8
lelsaesser <lucas.elsaesser@gmail.com>
2019-10-23 11:54:36 +0200

    Flask: fixed requirements.txt; Scripts: made bootscripts unix compatible

--- src/scripts/run_flask_backend.sh
+++ src/scripts/run_flask_backend.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Windows version
 
 export FLASK_APP="../GM-Manager-Flask/app.py"
 source /c/Anaconda3/etc/profile.d/conda.sh
+source /opt/anaconda3/etc/profile.d/conda.sh
+conda init bash
 conda activate GM-Manager-Flask
+pip install -r "../GM-Manager-Flask/requirements.txt"
 flask run -h 0.0.0.0 # runs on 127.0.0.1:5000
