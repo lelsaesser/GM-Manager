65ff7d9e4110dda0a7623c56c43093c4b9e0f1e8
lelsaesser <lucas.elsaesser@gmail.com>
2019-10-23 16:14:00 +0200

    Angular: reworked shc ai battle generator feature

--- src/GM-Manager-Angular/src/app/stronghold/stronghold.component.ts
+++ src/GM-Manager-Angular/src/app/stronghold/stronghold.component.ts
@@ -9,7 +9,6 @@ import { API_URL, SHC_API } from './../env';
 })
 export class StrongholdComponent {
 
-  ai_count = 8;
   shcJson: JSON;
   SHC_API_URL = `${API_URL}` + `${SHC_API}`;
 
@@ -18,21 +17,20 @@ export class StrongholdComponent {
   fetchShcBattleData() {
     this.httpClient.get(this.SHC_API_URL).subscribe(data => {
       this.shcJson = data as JSON;
-      console.log("POST call successful value returned in body", data);
+      console.log("GET call successful value returned in body", data);
     },
       response => {
-        console.log("POST call in error", response);
+        console.log("GET call in error", response);
       },
       () => {
-        console.log("The POST observable is now completed.");
+        console.log("The GET observable is now completed.");
       });
   }
 
-  fetchShcBattleDataWithPlayerCount() {
-    console.log(this.ai_count)
+  fetchShcBattleDataWithPlayerCount(ai_count) {
     this.httpClient.post(this.SHC_API_URL,
       {
-        "shc_ai_battle_player_count": this.ai_count
+        "shc_ai_battle_player_count": ai_count
       }).subscribe(data => {
         this.shcJson = data as JSON
         console.log("POST call successful value returned in body", data);
