b99edece0b502f5bf72ff0bbb3da58e61460dd81
lelsaesser <lucas.elsaesser@gmail.com>
2019-10-23 16:43:19 +0200

    Flask: added shc post api integration test

--- src/GM-Manager-Flask/test/integration_tests/test_shc_api.py
+++ src/GM-Manager-Flask/test/integration_tests/test_shc_api.py
@@ -9,3 +9,11 @@ class TestShcApi:
 
         assert response is not None
         assert response.status_code == 200
+
+    def test_get_ai_battle_data_with_ai_count(self):
+        for ai_count in range(2, 9):
+            payload = {'shc_ai_battle_player_count': ai_count}
+            response = requests.post(constants.FLASK_BACKEND_URL + constants.API_STRONGHOLD_GET_AI_BATTLE, json=payload)
+
+            assert response is not None
+            assert response.status_code == 200
