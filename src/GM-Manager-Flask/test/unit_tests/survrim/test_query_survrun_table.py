e9a172d4b3a5d3deb76b0b6c58f0f22a63f40904
lelsaesser <lucas.elsaesser@gmail.com>
2019-10-29 16:34:35 +0100

    added all missing tests but test_delete

--- src/GM-Manager-Flask/test/unit_tests/survrim/test_query_survrun_table.py
+++ src/GM-Manager-Flask/test/unit_tests/survrim/test_query_survrun_table.py
@@ -6,13 +6,31 @@ from database import constants as db_constants
 class TestQuerySurvrunTable:
 
     def test_survrun_insert_query(self):
-        query_db = QuerySurvrunTable()
-        status, msg = query_db.survrun_insert_query(survrim_constants.CLASS_WARRIOR, survrim_constants.CITY_WINDHELM,
-                                                    survrim_constants.CITY_WHITERUN, 40, "yes", 32, 3,
-                                                    survrim_constants.DIFFICULTY_HARDCORE)
+        db_cursor = QuerySurvrunTable()
+        status, msg = db_cursor.survrun_insert_query(survrim_constants.CLASS_WARRIOR, survrim_constants.CITY_WINDHELM,
+                                                     survrim_constants.CITY_WHITERUN, 40, "yes", 32, 3,
+                                                     survrim_constants.DIFFICULTY_HARDCORE)
 
         assert status == 200
         assert msg == db_constants.SUCCESS_QUERY_COMPLETED
 
         # remove the created table record
-        query_db.survrun_delete_last_added_record_query()
+        db_cursor.survrun_delete_last_added_record_query()
+
+    def test_survrun_select_query(self):
+        db_cursor = QuerySurvrunTable()
+        data = db_cursor.survrun_select_query()
+
+        assert data is not None
+
+    def test_survrun_delete_last_added_record_query(self):
+        db_cursor = QuerySurvrunTable()
+
+        # create test entry and delete it
+        status, msg = db_cursor.survrun_insert_query(survrim_constants.CLASS_WARRIOR, survrim_constants.CITY_WINDHELM,
+                                                     survrim_constants.CITY_WHITERUN, 40, "yes", 32, 3,
+                                                     survrim_constants.DIFFICULTY_HARDCORE)
+        status, msg = db_cursor.survrun_delete_last_added_record_query()
+
+        assert status == 200
+        assert msg == db_constants.SUCCESS_QUERY_COMPLETED
