399343ddf17be5e51667bc37479acf057de8342f
Lucas Elsässer <lucas.elsaesser@gmail.com>
2020-01-28 14:49:36 +0100

    Added table and user creation to postgre bootscript

--- src/scripts/run_postgre_database.sh
+++ src/scripts/run_postgre_database.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then # mac os
+    /usr/local/opt/postgres/bin/createuser -s postgres # setup db for first time use
+    /usr/local/opt/postgres/bin/createdb gm-manager-db -U postgres # setup db for first time use
     pg_ctl -D /usr/local/var/postgres start
 
 elif [[ "$OSTYPE" == "msys" ]]; then # windows git bash
