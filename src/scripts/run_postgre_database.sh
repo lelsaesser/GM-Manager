2cdb25c78054b09199cd266ebab5bfdad6205499
Lucas Elsässer <lucas.elsaesser@gmail.com>
2020-01-28 14:55:41 +0100

    follow up fix

--- src/scripts/run_postgre_database.sh
+++ src/scripts/run_postgre_database.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then # mac os
+    pg_ctl -D /usr/local/var/postgres start
     /usr/local/opt/postgres/bin/createuser -s postgres # setup db for first time use
     /usr/local/opt/postgres/bin/createdb gm-manager-db -U postgres # setup db for first time use
-    pg_ctl -D /usr/local/var/postgres start
 
 elif [[ "$OSTYPE" == "msys" ]]; then # windows git bash
     echo "windows witg git bash"
